1e6c69477994c6ba4fe9f9be11481bc7223be3ee
Zaeem Ahmed <zaeemahmed@gmail.com>
2026-01-08 22:44:52 +0500

    OK

--- index.js
+++ index.js
@@ -18,8 +18,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    "https://url-shortener-website-frontend.vercel.app",
+    "http://localhost:5173",
+  ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  exposedHeaders: ['Set-Cookie'],
+  secure: process.env.NODE_ENV === 'production',
 }));
 
 
